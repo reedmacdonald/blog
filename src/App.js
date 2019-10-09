e4aa0fa7c2cb1204d9f9052523cbe3147621acbc
Reed MacDonald <reedpmacdonald@gmail.com>
2019-10-09 01:59:17 -0700

    idk

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import CornerOne from './Pictures/buildingCornerOne.png'
 import Apartment from './Pictures/apartment.png'
 import Cube from './Pictures/cube.png'
 import ProfilePic from './Pictures/profilePic.png'
+import Part1 from './Pictures/part1.png'
+import Part2 from './Pictures/part2.png'
+import Part4 from './Pictures/part4.png'
+import Part5 from './Pictures/part5.png'
+import Part6 from './Pictures/part6.png'
+import Part7 from './Pictures/part7.png'
 
 
 
@@ -32,13 +38,13 @@ class App extends Component{
       <div className='bar' id='bar9'><img id='cube' src={Cube}></img></div>
       <div className='bar' id='bar10'><img id='profilePic' src={ProfilePic}></img></div>
     </div>
-    <div id='div2' className='article'><h5>Interior</h5><h1 className='titles'>9 Air-Cleaning Plants Your Home Needs</h1><br/><button id='navyButton'><b>Read for $0.09</b></button><img id='plant' className='artImg' src={Plant}></img></div>
-    <div id='div3' className='article'><h5>Food</h5><h1 className='titles'>One Month Sugar Detox</h1><br/><button id='pinkButton'><b>Read for $0.99</b></button><img id='otherHand' className='artImg' src={OtherHand}></img></div>
-    <div id='div4' className='article'><h5>Photography</h5><h1 className='titles'>Shooting Minimal Instagram Photos</h1><br/><button id='blueButton'><b>Read for $0.29</b></button><img className='artImg' id='apartment' src={Apartment}></img></div>
+    <div id='div2' className='article'><h5>Interior</h5><h1 className='titles'>9 Air-Cleaning Plants Your Home Needs</h1><br/><button id='navyButton'><b>Read for $0.09</b></button><img id='plant' className='artImg' src={Part5}></img></div>
+    <div id='div3' className='article'><h5>Food</h5><h1 className='titles'>One Month Sugar Detox</h1><br/><button id='pinkButton'><b>Read for $0.99</b></button><img id='otherHand' className='artImg' src={Part6}></img></div>
+    <div id='div4' className='article'><h5>Photography</h5><h1 className='titles'>Shooting Minimal Instagram Photos</h1><br/><button id='blueButton'><b>Read for $0.29</b></button><img className='artImg' id='apartment' src={Part7}></img></div>
     <div id='div5' ><h1><b>Lifestyle.</b></h1><h4>The latest and best lifestyle articles selected by our editorial staff</h4></div>
-    <div id='div6' className='article'><h5>Architecture</h5><h1 className='titles'>The Brand New NASA Office</h1><br/><button className='topRight' id='blackButton'><b>Read for $0.19</b></button><img className='artImg' id='cornerOne' src={CornerOne}></img></div>
-    <div id='div7' className='article'><h5>Travel</h5><h1  className='titles'>Experience the Saharan Sands</h1><br/><button  id='tanButton' className='topRight'><b>Read for $0.29</b></button><img className='artImg' id='hand' src={Hand}></img></div>
-    <div id='div8' className='article'><h5>Food</h5><h1 className='titles'>Wake Up and Smell the Coffee</h1><br/><button id='greenButton'><b>Read for $0.99</b></button><img className='artImg' id='cup' src={Cup}></img></div>
+    <div id='div6' className='article'><h5>Architecture</h5><h1 className='titles'>The Brand New NASA Office</h1><br/><button className='topRight' id='blackButton'><b>Read for $0.19</b></button><img className='artImg' id='cornerOne' src={Part2}></img></div>
+    <div id='div7' className='article'><h5>Travel</h5><h1  className='titles'>Experience the Saharan Sands</h1><br/><button  id='tanButton' className='topRight'><b>Read for $0.29</b></button><img className='artImg' id='hand' src={Part4}></img></div>
+    <div id='div8' className='article'><h5>Food</h5><h1 className='titles'>Wake Up and Smell the Coffee</h1><br/><button id='greenButton'><b>Read for $0.99</b></button><img className='artImg' id='cup' src={Part1}></img></div>
   </div>
     )
   }
